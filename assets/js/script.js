b4afe08ec0bae33b40e4ba4305ecda0529b94352
Marko Stankovic <stankovicm.srb@gmail.com>
2024-05-18 02:12:27 +0200

    changes to laoding bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+//loader START
+$(window).on('load', function() {
 
+    // Ensure the minimum loading time of 2 seconds
+    setTimeout(function() {
+        $('#loading-screen').fadeOut('slow', function() {
+            $('#main-content').fadeIn('slow'); // show content
+            $('body, html').css('overflow', 'auto'); // re-enable scrolling
+        });
+    }, 2000); // 2 seconds minimum
+    
+});
+//loader END
