a0cc9025a8469db78c475f8888ee8725c4192c51
Marko Stankovic <stankovicm.srb@gmail.com>
2024-05-30 21:56:08 +0200

    modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(window).on('load', function() {
     // Ensure the minimum loading time of 2 seconds
     setTimeout(function() {
         $('#loading-screen').fadeOut('slow', function() {
-            $('#main-content').fadeIn('slow'); // show content
+            $('#main').fadeIn('slow'); // show content
             $('body, html').css('overflow', 'auto'); // re-enable scrolling
         });
     }, 2000); // 2 seconds minimum
